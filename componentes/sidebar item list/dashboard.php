f4b13c856e1a1af78b17aede9c5e05833d32603c
QuiJess <quijess6@gmail.com>
2022-05-06 14:07:07 -0400

    Revert "Creacion de plantilla de dashboard y manipulacion de id de javascript"
    This reverts commit e274f0c066b17106c35038ee4ce7893922f0d378.

--- componentes/sidebar item list/dashboard.php
+++ componentes/sidebar item list/dashboard.php
@@ -2,11 +2,6 @@
 <!-- DASHBOARD ITEM -->
 
 <div class="p-2 hover:bg-pink-100">
-            <a
-            
-            href="?pagina=dashboard"
-            >
-        
               <div class="flex flex-row space-x-3">
                 <div class="h-6 w-6 text-pink-700">
                 <i class="text-2xl fa-solid fa-house"></i>
@@ -14,5 +9,4 @@
                 <h4 class="font-bold text-gray-500 
                 hover:text-pink-500">Dashboard</h4>
             </div>
-            </a>
  </div>
