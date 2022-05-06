213bff4c3eeb0f72a044e63c0f976a87a3b68dd7
QuiJess <quijess6@gmail.com>
2022-05-06 14:38:36 -0400

    Modificacion side bar, agregando nuevo item y cambiando iconos

--- componentes/sidebar item list/servicios.php
+++ componentes/sidebar item list/servicios.php
@@ -3,10 +3,9 @@
 <div id="mostrar" class="p-2 hover:bg-pink-100">
               <div class="flex flex-row space-x-3">
                 <div class="h-6 w-6 text-pink-700">
-                <i class="ml-1 text-2xl fa-solid fa-prescription-bottle-medical"></i>
-                
+                <i class="text-2xl fa-solid fa-toolbox"></i>
                 </div>
                 <h4  class="font-bold text-gray-500 
-                hover:text-pink-500">Servicios</h4> 
+                hover:text-pink-500">Administrar</h4> 
             </div>
           </div>
