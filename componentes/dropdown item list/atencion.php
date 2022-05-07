0237e3263e209845ede8284aa461593afb008277
QuiJess <quijess6@gmail.com>
2022-05-07 12:22:18 -0400

    Sidebar, agregando dropdown a atencion

---
+++ componentes/dropdown item list/atencion.php
@@ -0,0 +1,87 @@
+<!-- DROPDOWN SERVICIOS ITEM -->
+
+<div id="submenu" class="p-2 hover:bg-pink-100">
+              <div class="flex flex-row space-x-3">
+                
+                  <ul class="relative">
+    <li class="relative" id="mostrar_empleados">
+      <a class="flex items-center text-sm py-4 px-6 h-12 overflow-hidden text-gray-700 text-ellipsis whitespace-nowrap rounded hover:text-white hover:bg-black  transition duration-300 ease-in-out cursor-pointer" >
+      <i class="fa-solid fa-user-doctor"></i>
+        <span>Afiliacion</span>
+        <i class="ml-2 fa-solid fa-angle-down w-3 h-3 "></i>
+      </a>
+      <ul class="relative accordion-collapse collapse" id="empleadossub" >
+        <li class="relative">
+          <a href="?pagina=servicios" class="flex items-center text-xs py-4 pl-12 pr-6 h-6 overflow-hidden text-gray-700 text-ellipsis whitespace-nowrap rounded hover:text-white hover:bg-black  transition duration-300 ease-in-out" >Listar Empleados</a>
+        </li>
+        <li class="relative">
+          <a href="#!" class="flex items-center text-xs py-4 pl-12 pr-6 h-6 overflow-hidden text-gray-700 text-ellipsis whitespace-nowrap rounded hover:text-white hover:bg-black  transition duration-300 ease-in-out" >Registrar Empleados</a>
+        </li>
+        <li class="relative">
+          <a href="#!" class="flex items-center text-xs py-4 pl-12 pr-6 h-6 overflow-hidden text-gray-700 text-ellipsis whitespace-nowrap rounded hover:text-white hover:bg-black  transition duration-300 ease-in-out" >Reporte de Empleados</a>
+        </li>
+        
+      </ul>
+    </li>
+    <li class="relative" id="mostrar_servicios">
+      <a class="flex items-center text-sm py-4 px-6 h-12 overflow-hidden text-gray-700 text-ellipsis whitespace-nowrap rounded hover:text-white hover:bg-black  transition duration-300 ease-in-out cursor-pointer" >
+      <i class="fa-solid fa-file-medical"></i>
+
+        <span>Plan de salud</span>
+        <i class="ml-2 fa-solid fa-angle-down w-3 h-3 "></i>
+      </a>
+      <ul class="relative accordion-collapse collapse" id="serviciossub">
+        <li class="relative">
+          <a href="#!" class="flex items-center text-xs py-4 pl-12 pr-6 h-6 overflow-hidden text-gray-700 text-ellipsis whitespace-nowrap rounded hover:text-white hover:bg-black  transition duration-300 ease-in-out" >Listar servicios</a>
+        </li>
+        <li class="relative">
+          <a href="#!" class="flex items-center text-xs py-4 pl-12 pr-6 h-6 overflow-hidden text-gray-700 text-ellipsis whitespace-nowrap rounded hover:text-white hover:bg-black  transition duration-300 ease-in-out" >Registro de servicios</a>
+        </li>
+        <li class="relative">
+          <a href="#!" class="flex items-center text-xs py-4 pl-12 pr-6 h-6 overflow-hidden text-gray-700 text-ellipsis whitespace-nowrap rounded hover:text-white hover:bg-black  transition duration-300 ease-in-out" >Reporte de servicios</a>
+        </li>
+      </ul>
+    </li>
+    <li class="relative" id="mostrar_servicios">
+      <a class="flex items-center text-sm py-4 px-6 h-12 overflow-hidden text-gray-700 text-ellipsis whitespace-nowrap rounded hover:text-white hover:bg-black  transition duration-300 ease-in-out cursor-pointer" >
+      <i class="fa-solid fa-file-medical"></i>
+
+        <span>Contratos</span>
+        <i class="ml-2 fa-solid fa-angle-down w-3 h-3 "></i>
+      </a>
+      <ul class="relative accordion-collapse collapse" id="serviciossub">
+        <li class="relative">
+          <a href="#!" class="flex items-center text-xs py-4 pl-12 pr-6 h-6 overflow-hidden text-gray-700 text-ellipsis whitespace-nowrap rounded hover:text-white hover:bg-black  transition duration-300 ease-in-out" >Listar servicios</a>
+        </li>
+        <li class="relative">
+          <a href="#!" class="flex items-center text-xs py-4 pl-12 pr-6 h-6 overflow-hidden text-gray-700 text-ellipsis whitespace-nowrap rounded hover:text-white hover:bg-black  transition duration-300 ease-in-out" >Registro de servicios</a>
+        </li>
+        <li class="relative">
+          <a href="#!" class="flex items-center text-xs py-4 pl-12 pr-6 h-6 overflow-hidden text-gray-700 text-ellipsis whitespace-nowrap rounded hover:text-white hover:bg-black  transition duration-300 ease-in-out" >Reporte de servicios</a>
+        </li>
+      </ul>
+    </li>
+    <li class="relative" id="mostrar_servicios">
+      <a class="flex items-center text-sm py-4 px-6 h-12 overflow-hidden text-gray-700 text-ellipsis whitespace-nowrap rounded hover:text-white hover:bg-black  transition duration-300 ease-in-out cursor-pointer" >
+      <i class="fa-solid fa-file-medical"></i>
+
+        <span>Pagos</span>
+        <i class="ml-2 fa-solid fa-angle-down w-3 h-3 "></i>
+      </a>
+      <ul class="relative accordion-collapse collapse" id="serviciossub">
+        <li class="relative">
+          <a href="#!" class="flex items-center text-xs py-4 pl-12 pr-6 h-6 overflow-hidden text-gray-700 text-ellipsis whitespace-nowrap rounded hover:text-white hover:bg-black  transition duration-300 ease-in-out" >Listar servicios</a>
+        </li>
+        <li class="relative">
+          <a href="#!" class="flex items-center text-xs py-4 pl-12 pr-6 h-6 overflow-hidden text-gray-700 text-ellipsis whitespace-nowrap rounded hover:text-white hover:bg-black  transition duration-300 ease-in-out" >Registro de servicios</a>
+        </li>
+        <li class="relative">
+          <a href="#!" class="flex items-center text-xs py-4 pl-12 pr-6 h-6 overflow-hidden text-gray-700 text-ellipsis whitespace-nowrap rounded hover:text-white hover:bg-black  transition duration-300 ease-in-out" >Reporte de servicios</a>
+        </li>
+      </ul>
+    </li>
+    
+      </ul>
+
+            </div>
+          </div>
