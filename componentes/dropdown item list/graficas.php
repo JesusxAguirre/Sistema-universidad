cdca2458ed6f50497a8bab618be9820ae778856b
QuiJess <quijess6@gmail.com>
2022-05-08 11:10:39 -0400

    Sidebar, Cambiando iconos

--- componentes/dropdown item list/graficas.php
+++ componentes/dropdown item list/graficas.php
@@ -6,41 +6,19 @@
                   <ul class="relative">
     <li class="relative" id="mostrar_empleados">
       <a class="flex items-center text-sm py-4 px-6 h-12 overflow-hidden text-gray-700 text-ellipsis whitespace-nowrap rounded hover:text-white hover:bg-black  transition duration-300 ease-in-out cursor-pointer" >
-      <i class="fa-solid fa-user-doctor"></i>
+      <i class="text-2xl fa-solid fa-chart-pie"></i>
         <span>Graficas I</span>
-        <i class="ml-2 fa-solid fa-angle-down w-3 h-3 "></i>
+       
       </a>
-      <ul class="relative accordion-collapse collapse" id="empleadossub" >
-        <li class="relative">
-          <a href="?pagina=servicios" class="flex items-center text-xs py-4 pl-12 pr-6 h-6 overflow-hidden text-gray-700 text-ellipsis whitespace-nowrap rounded hover:text-white hover:bg-black  transition duration-300 ease-in-out" >Listar Empleados</a>
-        </li>
-        <li class="relative">
-          <a href="#!" class="flex items-center text-xs py-4 pl-12 pr-6 h-6 overflow-hidden text-gray-700 text-ellipsis whitespace-nowrap rounded hover:text-white hover:bg-black  transition duration-300 ease-in-out" >Registrar Empleados</a>
-        </li>
-        <li class="relative">
-          <a href="#!" class="flex items-center text-xs py-4 pl-12 pr-6 h-6 overflow-hidden text-gray-700 text-ellipsis whitespace-nowrap rounded hover:text-white hover:bg-black  transition duration-300 ease-in-out" >Reporte de Empleados</a>
-        </li>
-        
-      </ul>
     </li>
     <li class="relative" id="mostrar_servicios">
       <a class="flex items-center text-sm py-4 px-6 h-12 overflow-hidden text-gray-700 text-ellipsis whitespace-nowrap rounded hover:text-white hover:bg-black  transition duration-300 ease-in-out cursor-pointer" data-bs-target="#collapseSidenavEx2" aria-expanded="false" aria-controls="collapseSidenavEx2">
-      <i class="fa-solid fa-file-medical"></i>
+      <i class="text-2xl fa-solid fa-chart-pie"></i>
 
         <span>Graficas II</span>
-        <i class="ml-2 fa-solid fa-angle-down w-3 h-3 "></i>
+        
       </a>
-      <ul class="relative accordion-collapse collapse" id="serviciossub">
-        <li class="relative">
-          <a href="#!" class="flex items-center text-xs py-4 pl-12 pr-6 h-6 overflow-hidden text-gray-700 text-ellipsis whitespace-nowrap rounded hover:text-white hover:bg-black  transition duration-300 ease-in-out" >Listar servicios</a>
-        </li>
-        <li class="relative">
-          <a href="#!" class="flex items-center text-xs py-4 pl-12 pr-6 h-6 overflow-hidden text-gray-700 text-ellipsis whitespace-nowrap rounded hover:text-white hover:bg-black  transition duration-300 ease-in-out" >Registro de servicios</a>
-        </li>
-        <li class="relative">
-          <a href="#!" class="flex items-center text-xs py-4 pl-12 pr-6 h-6 overflow-hidden text-gray-700 text-ellipsis whitespace-nowrap rounded hover:text-white hover:bg-black  transition duration-300 ease-in-out" >Reporte de servicios</a>
-        </li>
-      </ul>
+      
     </li>
     
       </ul>
