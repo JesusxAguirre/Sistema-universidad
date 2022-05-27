121f50ce9284f418f1d1cc5081a30a1687252bc3
JesusxAguirre <99jesus.aguirre@gmail.com>
2022-05-27 10:06:10 -0400

    Cambiando hover styles en dropdown atencion

--- componentes/sidebar item list/dashboard.php
+++ componentes/sidebar item list/dashboard.php
@@ -1,13 +1,10 @@
-
 <!-- DASHBOARD ITEM -->
 
 <div class="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-500 text-white">
-            <a
-            href="?pagina=dashboard"
-            >
-                <i class="lg:text-2xl md:text-base text-sm fa-solid fa-house"></i>
-              
-                <span class="text-[15px] ml-4 text-gray-200 ">Dashboard</span>
-            
-            </a>
- </div>
+  <a href="?pagina=dashboard">
+    <i class="lg:text-2xl md:text-base text-sm fa-solid fa-house"></i>
+
+    <span class="text-[15px] ml-4 text-gray-200 ">Dashboard</span>
+
+  </a>
+</div>
