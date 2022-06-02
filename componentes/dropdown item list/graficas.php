930e3f96dc06897e0beb067abb049207d641e7a7
QuiJess <quijess6@gmail.com>
2022-06-02 08:25:59 -0400

    Formateando archivos de la carpeta dropdown items

--- componentes/dropdown item list/graficas.php
+++ componentes/dropdown item list/graficas.php
@@ -1,27 +1,31 @@
 <!-- DROPDOWN GRAFICAS ITEM -->
 
-<div id="graficassub" class="text-left text-sm font-thin mt-2 w-4/5 mx-auto text-gray-200">
-              <div class="flex flex-row space-x-3">
-                
-                  <ul class="relative">
-    <li class="relative" id="mostrar_empleados">
-      <a class="flex items-center text-sm py-4 px-6 h-12 overflow-hidden  text-ellipsis whitespace-nowrap rounded hover:text-white hover:bg-gray-700  transition duration-300 ease-in-out cursor-pointer" >
-      <i class="text-2xl fa-solid fa-chart-pie"></i>
-        <span>Graficas I</span>
-       
-      </a>
-    </li>
-    <li class="relative" id="mostrar_servicios">
-      <a class="flex items-center text-sm py-4 px-6 h-12 overflow-hidden  text-ellipsis whitespace-nowrap rounded hover:text-white hover:bg-gray-700  transition duration-300 ease-in-out cursor-pointer" data-bs-target="#collapseSidenavEx2" aria-expanded="false" aria-controls="collapseSidenavEx2">
-      <i class="text-2xl fa-solid fa-chart-pie"></i>
+<div
+  id="graficassub"
+  class="text-left text-sm font-thin mt-2 w-4/5 mx-auto text-gray-200"
+>
+  <div class="flex flex-row space-x-3">
+    <ul class="relative">
+      <li class="relative" id="mostrar_empleados">
+        <a
+          class="flex items-center text-sm py-4 px-6 h-12 overflow-hidden text-ellipsis whitespace-nowrap rounded hover:text-white hover:bg-gray-700 transition duration-300 ease-in-out cursor-pointer"
+        >
+          <i class="text-2xl fa-solid fa-chart-pie"></i>
+          <span>Graficas I</span>
+        </a>
+      </li>
+      <li class="relative" id="mostrar_servicios">
+        <a
+          class="flex items-center text-sm py-4 px-6 h-12 overflow-hidden text-ellipsis whitespace-nowrap rounded hover:text-white hover:bg-gray-700 transition duration-300 ease-in-out cursor-pointer"
+          data-bs-target="#collapseSidenavEx2"
+          aria-expanded="false"
+          aria-controls="collapseSidenavEx2"
+        >
+          <i class="text-2xl fa-solid fa-chart-pie"></i>
 
-        <span>Graficas II</span>
-        
-      </a>
-      
-    </li>
-    
-      </ul>
-
-            </div>
-          </div>
+          <span>Graficas II</span>
+        </a>
+      </li>
+    </ul>
+  </div>
+</div>
