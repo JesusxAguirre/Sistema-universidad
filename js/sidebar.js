40c8081135171127d0a2b9e4aa863211b5337755
QuiJess <quijess6@gmail.com>
2022-06-02 07:38:02 -0400

    Finalizando instalacion  eslint al mismo tiempo acomodando formato en sidebar.js

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,112 +1,86 @@
-$(document).ready(function(){
-    
-    //aqui va el codigo javascript
-        //OCULTAMIENTO
-                $("#submenu").hide();
-                $("#submenu2").hide();
-            
-            
-                $("#empleadossub").hide();
-                $("#serviciossub").hide();
-                
-            
-            
-                //ATENCION
-                $("#atencionsub").hide();
-                $("#afiliacionsub").hide();
-            
-                $("#plansub").hide();
-                $("#contratossub").hide();
-                $("#pagossub").hide();
-            
-       
-                //GRAFICAS
-                $("#graficassub").hide();
-                $("#afiliacionsub").hide();
+/* eslint-disable no-undef */
+/* eslint-disable camelcase */
+// Funciones
 
-
-
-
-
-        //FIN OCULTAMIENTO
-       
-       //LLAMADA DE FUNCIONES TOOGLE
-
-
-        //SERVICIOS
-        $("#mostrar").click(mostrar);
-          
-        $("#mostrar_empleados").click(mostrar_empleados);
-
-        $("#mostrar_servicios").click(mostrar_servicios);
-        //FIN SERVICIOS
-
-
-        //ATENCION
-        $("#mostrar_atencion").click(mostrar_atencion);
-            
-        $("#mostrar_afiliacion").click(mostrar_afiliacion);
-
-        $("#mostrar_plan").click(mostrar_plan);
-
-        $("#mostrar_contratos").click(mostrar_contratos);
-
-        $("#mostrar_pagos").click(mostrar_pagos);
-
-
-
-
-          //FIN ATENCION
-
-          
-        //GRAFICAS
-       
-        $("#mostrar_graficas").click(mostrar_graficas);
-
-
-          //FIN GRAFICAS
-        //sidebar
-       
-
- 
- });
- 
-  //Funciones
- 
-function mostrar(){
- $("#submenu").toggle();
-
-
+function mostrar() {
+  $('#submenu').toggle();
 }
-function mostrar2(){
-    $("#submenu2").toggle();
-   
-   
-   }
-function mostrar_empleados(){
-    $("#empleadossub").toggle();
-   
-   
-   }
-function mostrar_servicios(){
-    $("#serviciossub").toggle();
-}
-function mostrar_atencion(){
-    $("#atencionsub").toggle();
-}  
-function mostrar_afiliacion(){
-    $("#afiliacionsub").toggle();
-}  
-function mostrar_plan(){
-    $("#plansub").toggle();
-}  
-function mostrar_contratos(){
-    $("#contratossub").toggle();
-}  
-function mostrar_pagos(){
-    $("#pagossub").toggle();
-}  
-function mostrar_graficas(){
-    $("#graficassub").toggle();
-}  
 
+function mostrar_empleados() {
+  $('#empleadossub').toggle();
+}
+function mostrar_servicios() {
+  $('#serviciossub').toggle();
+}
+function mostrar_atencion() {
+  $('#atencionsub').toggle();
+}
+function mostrar_afiliacion() {
+  $('#afiliacionsub').toggle();
+}
+function mostrar_plan() {
+  $('#plansub').toggle();
+}
+function mostrar_contratos() {
+  $('#contratossub').toggle();
+}
+function mostrar_pagos() {
+  $('#pagossub').toggle();
+}
+function mostrar_graficas() {
+  $('#graficassub').toggle();
+}
+
+$(document).ready(() => {
+  // aqui va el codigo javascript
+  // OCULTAMIENTO
+  $('#submenu').hide();
+  $('#submenu2').hide();
+
+  $('#empleadossub').hide();
+  $('#serviciossub').hide();
+
+  // ATENCION
+  $('#atencionsub').hide();
+  $('#afiliacionsub').hide();
+
+  $('#plansub').hide();
+  $('#contratossub').hide();
+  $('#pagossub').hide();
+
+  // GRAFICAS
+  $('#graficassub').hide();
+  $('#afiliacionsub').hide();
+
+  // FIN OCULTAMIENTO
+
+  // LLAMADA DE FUNCIONES TOOGLE
+
+  // SERVICIOS
+  $('#mostrar').click(mostrar);
+
+  $('#mostrar_empleados').click(mostrar_empleados);
+
+  $('#mostrar_servicios').click(mostrar_servicios);
+  // FIN SERVICIOS
+
+  // ATENCION
+  $('#mostrar_atencion').click(mostrar_atencion);
+
+  $('#mostrar_afiliacion').click(mostrar_afiliacion);
+
+  $('#mostrar_plan').click(mostrar_plan);
+
+  $('#mostrar_contratos').click(mostrar_contratos);
+
+  $('#mostrar_pagos').click(mostrar_pagos);
+
+  // FIN ATENCION
+
+  // GRAFICAS
+
+  $('#mostrar_graficas').click(mostrar_graficas);
+
+  // FIN GRAFICAS
+  // sidebar
+});
